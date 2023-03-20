792c717b00554c35c652e3a583fa12a8ead97d91
Javier Serrano <javier.serrano@babelgroup.com>
2023-03-20 12:22:04 +0100

    Initial commit

---
+++ src/main/java/com/babel/Scoring/ScoringApplication.java
@@ -0,0 +1,13 @@
+package com.babel.Scoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/babel/Scoring/ScoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babel.Scoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
